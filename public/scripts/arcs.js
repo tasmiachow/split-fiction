73060f95d746eee5b5727ed79ba65d26955aa2b3
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-09-24 02:20:17 -0400

    cards created on frontend

--- public/scripts/arcs.js
+++ public/scripts/arcs.js
@@ -1,3 +1,37 @@
 const renderArcs = async () =>{
-
+    const response = await fetch('/arcs');
+    const data = await response.json();
+    const mainContent = document.querySelector('.main-content');
+    if(data){
+        data.map(arc => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const topContainer = document.createElement('div');
+            topContainer.classList.add('top-container');
+            const bottomContainer = document.createElement('div');
+            bottomContainer.classList.add('bottom-container');
+            topContainer.style.backgroundImage = `url(${arc.image})`;
+            const title = document.createElement('h3');
+            title.textContent = (`${arc.title}`);
+            bottomContainer.appendChild(title);
+            const author = document.createElement('p');
+            author.textContent = (`Author: ${arc.author}`);
+            bottomContainer.appendChild(author);
+            const link = document.createElement('a');
+            link.textContent = "Read More";
+            link.setAttribute('role', 'button');
+            link.href = `/arcs/${arc.id}`
+            bottomContainer.appendChild(link);
+            card.appendChild(topContainer);
+            card.appendChild(bottomContainer);
+            mainContent.appendChild(card);
+        })
+    }
+    else{
+        const message = document.createElement('h2');
+        message.textContent = "No stories available 😞"
+        mainContent.appendChild(message);
+    }
 }
+
+renderArcs();
